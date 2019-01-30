4de77306bc3f553d4d6d050db8f9dc6e74c8da4e
Kenneth M Doud <kmdoud@gmail.com>
2019-01-30 16:46:14 -0500

    Initial Commit

--- ClassLesson/Program.cs
+++ ClassLesson/Program.cs
@@ -6,10 +6,65 @@ using System.Threading.Tasks;
 
 namespace ClassLesson
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var chris = new Student();
+            var ken = new Student();
+            var kellan = new Student();
+            var seth = new Student();
+            var irenea = new Student();
+            var ben = new Student();
+            var sammy = new Student();
+
+            chris.FirstName = "Chris";
+            chris.LastName = "Caudill";
+            chris.Email = "christophercaudill@gmail.com";
+            chris.GPA = 3.9;
+            chris.SAT = 1493;
+            chris.Phone = "937-716-4027";
+            Console.WriteLine(chris.Print());
+            Console.WriteLine(chris.Print("New Print Method"));
+
+            var chrisfullname = chris.FullName();
+
+            ken.FirstName = "Kenneth";
+            ken.LastName = "Doud";
+            ken.Email = "kmdoud@gmail.com";
+            ken.Phone = "513-600-3890";
+            ken.GPA = 2.5;
+            ken.SAT = 1060;
+           Console.WriteLine(ken.Print());
+
+
+            kellan.FirstName = "Kellan";
+            kellan.LastName = "Arick";
+            kellan.Email = "kellan5015@gmail.com";
+            kellan.GPA = 3.8;
+            kellan.SAT = 1495;
+            kellan.Phone = "330-209-8167";
+            Console.WriteLine(kellan.Print());
+
+
+            seth.FirstName = "Seth";
+            seth.LastName = "Phillips";
+            seth.Email = "sethphillips042@gmail.com";
+            seth.GPA = 2.6;
+            seth.SAT = 1496;
+            seth.Phone = "513-335-3053";
+            Console.WriteLine(seth.Print());
+
+            sammy.FirstName = "Sam";
+            sammy.LastName = "Amare";
+            sammy.Email = "samrawit.amare@ymail.com";
+            sammy.Phone = "513-602-8450";
+            sammy.GPA = 3.9;
+            sammy.SAT = 1595;
+            Console.WriteLine(sammy.Print());
+
+
         }
     }
 }
---
+++ ClassLesson/Student.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLesson
+{
+    class Student
+    {
+        public string Prefix { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string _phone;
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                if (value.Length < 7 || value.Length > 12)
+                    Console.WriteLine("ERROR");
+                else
+                {
+                    _phone = value;
+                }
+            }
+        }
+        public string Email;
+        private double _GPA;
+        public double GPA
+        { get
+            {
+                return _GPA;
+            }
+            set
+            {
+                if (value < 0.0 || value > 5.0)
+                {
+                    Console.WriteLine("GPA Must be between 0 and 5");
+
+                }
+                else
+                {
+                    _GPA = value;
+                }
+            }
+        }
+        public int SAT { get; set; }
+        
+
+
+        public Student(string fname, string lname, string email, string phone, double gpa, int sat)
+        {
+            FirstName = fname;
+            LastName = lname;
+            Email = email;
+            Phone = phone;
+            GPA = gpa;
+            SAT = sat;
+
+        }
+        public Student()
+        {
+        }
+
+        public string FullName()
+        {
+            return $"{FirstName} {LastName}";
+
+        }
+        public string Print()
+        {
+            return $"{FirstName} {LastName} | {Phone}| {Email}| {GPA}| {SAT}|";
+        }
+
+        public string Print(string message)
+        {
+            return $"{message} :: " + Print();
+        }
+
+    }
+}
