4bd68ff9aa9f72967ad58a50161978b8f46d8702
Kenneth M Doud <kmdoud@gmail.com>
2019-01-30 11:02:34 -0500

    Add project files.

---
+++ ClassLesson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
